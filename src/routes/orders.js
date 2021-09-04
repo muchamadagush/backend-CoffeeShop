a2daa4cf8118d31362c0985588b61e4d681a5c64
Farrel Varian Eka Putra <42968960+farrelvarian@users.noreply.github.com>
2021-09-05 00:46:44 +0700

    initial order endpoint

---
+++ src/routes/orders.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const orderController = require("../controllers/orders");
+const auth = require("../middlewares/auth");
+
+router
+  .get("/", auth.verifyAccess, orderController.getAllOrder)
+  .get("/:id", auth.verifyAccess, orderController.getOrder)
+  .post("/", auth.verifyAccess, orderController.insertOrder)
+  .post("/orderdetail", auth.verifyAccess, orderController.insertOrderDetail)
+  .put("/:id", auth.verifyAccess, orderController.updateOrder)
+  .delete("/:id", auth.verifyAccess, orderController.deleteOrder);
+
+module.exports = router;
