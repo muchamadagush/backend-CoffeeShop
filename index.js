8916b20d6c6a7edc069c798b5f7f41d52d0dac6e
Muchamad Agus Hermawan <muchamadagush@gmail.com>
2021-09-08 19:11:01 +0700

    setting socket io

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const port = process.env.DB_PORT || 3500;
 const cors = require("cors");
 const createError = require("http-errors");
 const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+const http = require("http").createServer(app);
 
 // middleware
 
@@ -30,6 +32,71 @@ app.use("/users", userRouter);
 app.use("/", userAuthRouter);
 app.use("/files", express.static("./uploads"));
 
+// creating socket io instance
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.use((socket, next) => {
+  const token = socket.handshake.query.token;
+
+  jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+    if (err) {
+      if (err.name == "TokenExpiredError")
+        return res.status(403).send({ message: "Token expired" });
+      if (err.name == "JsonWebTokenError")
+        return res.status(403).send({ message: err.message });
+      if (err.name == "NotBeforeError")
+        return res.status(403).send({ message: "Token not active" });
+    }
+
+    socket.id = decoded.id;
+    socket.join(decoded.id);
+
+    next();
+  });
+});
+
+io.on("connection", function (socket) {
+  console.log("User connected", socket.id);
+
+  // attach incoming listener for new user
+  socket.on("userConnected", function (userId) {
+    // save in array
+    users[userId] = socket.id;
+
+    // socket id will be used to send message to individual person
+    // notify all connected clients
+    io.emit("userConnected", userId);
+  });
+
+  socket.on("sendMessage", (data, callback) => {
+    const dataMessage = {
+      id: uuid().split("-").join(""),
+      sender: socket.userId,
+      receiver: data.receiver,
+      message: data.message,
+      createdAt: new Date(),
+    };
+    callback({
+      ...dataMessage,
+      createdAt: moment(dataMessage.createdAt).format("LT"),
+    });
+    // modelChats.insertChat(dataMessage).then(() => {
+    //   socket.broadcast.to(dataMessage.receiver).emit("newMessage", {
+    //     ...dataMessage,
+    //     createdAt: moment(dataMessage.created_at).format("LT"),
+    //   });
+    // });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client terputus", socket.id);
+  });
+});
+
 app.use("*", (req, res, next) => {
   const error = new createError.NotFound();
   next(error);
@@ -42,6 +109,6 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(port, () => {
+http.listen(port, () => {
   console.log(`server is running on port ${port}`);
 });
